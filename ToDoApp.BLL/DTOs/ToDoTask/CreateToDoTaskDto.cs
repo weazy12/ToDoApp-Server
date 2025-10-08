21542b46a90e990ccfcaa8a3d650a4baf16b2c32
Valentyn Humen <valentin34561@gmail.com>
2025-10-08 23:56:32 +0300

    Add DTOs for endpoints

---
+++ ToDoApp.BLL/DTOs/ToDoTask/CreateToDoTaskDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.DAL.Enums;
+
+namespace ToDoApp.BLL.DTOs.ToDoTask
+{
+    public class CreateToDoTaskDto
+    {
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+
+        public DateTime DueDate { get; set; }
+
+    }
+}
---
+++ ToDoApp.BLL/DTOs/ToDoTask/ToDoTaskDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.DAL.Enums;
+
+namespace ToDoApp.BLL.DTOs.ToDoTask
+{
+    public class ToDoTaskDto
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public Status Status { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime CompletedAt { get; set; }
+    }
+}
