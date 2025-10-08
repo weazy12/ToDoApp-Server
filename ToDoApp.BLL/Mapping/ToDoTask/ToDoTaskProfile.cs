08f22f45b084a6c1bf65cb88216e945c1f8bfb0c
Valentyn Humen <valentin34561@gmail.com>
2025-10-08 23:59:29 +0300

    Add Mappings

---
+++ ToDoApp.BLL/Mapping/ToDoTask/ToDoTaskProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ToDoApp.BLL.DTOs.ToDoTask;
+using ToDoApp.DAL.Entities;
+
+namespace ToDoApp.BLL.Mapping.Tasks
+{
+    public class ToDoTaskProfile : Profile
+    {
+        public ToDoTaskProfile()
+        {
+            CreateMap<CreateToDoTaskDto, ToDoTask>();
+            CreateMap<UpdateToDoTaskDto, ToDoTask>();
+            CreateMap<ToDoTask, ToDoTaskDto>();
+        }
+    }
+}
