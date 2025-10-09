00e2653707101c97be4520af99ca7db195c25cbe
Valentyn Humen <valentin34561@gmail.com>
2025-10-09 14:58:01 +0300

    Add Controllers

---
+++ ToDoApp-Server/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.BLL.Mediatr.ResultVariations;
+
+namespace ToDoApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>()!;
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                if (result is NullResult<T>)
+                {
+                    return Ok(result.Value);
+                }
+
+                return (result.Value is null) ?
+                    NotFound("Found result matching null") : Ok(result.Value);
+            }
+
+            return BadRequest(result.Reasons);
+        }
+    }
+}
---
+++ ToDoApp-Server/Controllers/ToDoTasks/ToDoTasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.BLL.DTOs.ToDoTask;
+using ToDoApp.BLL.Mediatr.ToDoTask.Create;
+using ToDoApp.BLL.Mediatr.ToDoTask.Delete;
+using ToDoApp.BLL.Mediatr.ToDoTask.GetAll;
+using ToDoApp.BLL.Mediatr.ToDoTask.Update;
+
+namespace ToDoApp.WebApi.Controllers.ToDoTasks
+{
+    public class ToDoTasksController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllTask()
+        {
+            return HandleResult(await Mediator.Send(new GetAllToDoTasksQuery()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] CreateToDoTaskDto createToDoTaskDto)
+        {
+            return HandleResult(await Mediator.Send(new CreateToDoTaskCommand(createToDoTaskDto)));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateToDoTaskDto updateToDoTaskDto)
+        {
+            return HandleResult(await Mediator.Send(new UpdateToDoTaskCommand(updateToDoTaskDto)));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTask([FromBody] int id)
+        {
+            return HandleResult(await Mediator.Send(new DeleteToDoTaskCommand(id)));
+        }
+    }
+}
