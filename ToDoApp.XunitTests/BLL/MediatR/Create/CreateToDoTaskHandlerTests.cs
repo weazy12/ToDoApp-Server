155d0847c21e149a40ac8c09ce9c6d75c80d84bf
Valentyn Humen <valentin34561@gmail.com>
2025-10-15 00:59:12 +0300

    Add units test for Create and GetAll handlers

--- ToDoApp.XunitTests/BLL/MediatR/Create/CreateToDoTaskHandlerTests.cs
+++ ToDoApp.XunitTests/BLL/MediatR/Create/CreateToDoTaskHandlerTests.cs
@@ -63,9 +63,61 @@ namespace ToDoApp.XunitTests.BLL.MediatR.Create
             _mockRepositoryWrapper.Setup(r => r.SaveChangesAsync())
                 .ReturnsAsync(1);
 
+            _mockMapper.Setup(r => r.Map<ToDoTaskDto>(entity)).Returns(returnDto);
+
             var result = await _handler.Handle(new CreateToDoTaskCommand(createDto), CancellationToken.None);
 
             result.IsSuccess.Should().BeTrue();
+            _mockMapper.Verify(m => m.Map<DAL.Entities.ToDoTask>(createDto), Times.Once);
+            _mockRepositoryWrapper.Verify(r => r.ToDoTaskRepository.CreateAsync(entity), Times.Once);
+            _mockRepositoryWrapper.Verify(r => r.SaveChangesAsync(), Times.Once);
+            _mockMapper.Verify(r => r.Map<ToDoTaskDto>(entity), Times.Once);
+        }
+        [Fact]
+        public async Task Handle_ShouldReturnFailResult_WhileSaving()
+        {
+            var createDto = new CreateToDoTaskDto
+            {
+                Title = "Title",
+                Description = "Lorem ipsum",
+                DueDate = DateTime.UtcNow.AddDays(3)
+            };
+
+            var entity = new DAL.Entities.ToDoTask
+            {
+                Id = 1,
+                Title = "Title",
+                Description = "Lorem ipsum",
+                DueDate = DateTime.UtcNow.AddDays(3),
+                Status = DAL.Enums.Status.ToDo,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            var returnDto = new ToDoTaskDto
+            {
+                Id = 1,
+                Title = "Title",
+                Description = "Lorem ipsum",
+                DueDate = DateTime.UtcNow.AddDays(3),
+                Status = DAL.Enums.Status.ToDo,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _mockMapper.Setup(r => r.Map<DAL.Entities.ToDoTask>(createDto)).Returns(entity);
+
+            _mockRepositoryWrapper.Setup(r => r.ToDoTaskRepository.CreateAsync(entity))
+                .ReturnsAsync(entity);
+
+            _mockRepositoryWrapper.Setup(r => r.SaveChangesAsync())
+                .ReturnsAsync(0);
+
+            var result = await _handler.Handle(new CreateToDoTaskCommand(createDto), CancellationToken.None);
+
+            result.IsSuccess.Should().BeFalse();
+            _mockMapper.Verify(m => m.Map<DAL.Entities.ToDoTask>(createDto), Times.Once);
+            _mockRepositoryWrapper.Verify(r => r.ToDoTaskRepository.CreateAsync(entity), Times.Once);
+            _mockRepositoryWrapper.Verify(r => r.SaveChangesAsync(), Times.Once);
+            _mockMapper.Verify(r => r.Map<ToDoTaskDto>(entity), Times.Never);
         }
     }
 }
---
+++ ToDoApp.XunitTests/BLL/MediatR/GetAll/GetAllTodoTaskHanlderTests.cs
@@ -0,0 +1,112 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using ToDoApp.BLL.DTOs.ToDoTask;
+using ToDoApp.BLL.Mediatr.ToDoTask.GetAll;
+using ToDoApp.DAL.Repositories.Interfaces.Base;
+
+namespace ToDoApp.XunitTests.BLL.MediatR.GetAll
+{
+    public class GetAllTodoTaskHanlderTests
+    {
+        private readonly Mock<IRepositoryWrapper> _mockRepositoryWrapper;
+
+        private readonly Mock<IMapper> _mockMapper;
+
+        private readonly GetAllToDoTasksHandler _handler;
+        public GetAllTodoTaskHanlderTests()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockRepositoryWrapper = new Mock<IRepositoryWrapper>();
+            _handler = new GetAllToDoTasksHandler(_mockMapper.Object, _mockRepositoryWrapper.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnOkResult_WhenGetAllWasWorkedSuccess()
+        {
+            var entities = new List<DAL.Entities.ToDoTask>
+            {
+                new DAL.Entities.ToDoTask
+                {
+                    Id = 1,
+                    Title = "Title",
+                    Description = "Lorem ipsum",
+                    DueDate = DateTime.UtcNow.AddDays(3),
+                    Status = DAL.Enums.Status.ToDo,
+                    CreatedAt = DateTime.UtcNow,
+                },
+                new DAL.Entities.ToDoTask
+                {
+                    Id = 2,
+                    Title = "Title",
+                    Description = "Lorem ipsum1",
+                    DueDate = DateTime.UtcNow.AddDays(4),
+                    Status = DAL.Enums.Status.ToDo,
+                    CreatedAt = DateTime.UtcNow,
+                }
+            };
+
+            var dtos = new List<ToDoTaskDto>
+            {
+                new ToDoTaskDto
+                {
+                    Id = 1,
+                    Title = "Title",
+                    Description = "Lorem ipsum",
+                    DueDate = DateTime.UtcNow.AddDays(3),
+                    Status = DAL.Enums.Status.ToDo,
+                    CreatedAt = DateTime.UtcNow,
+                },
+                new ToDoTaskDto
+                {
+                    Id = 2,
+                    Title = "Title",
+                    Description = "Lorem ipsum1",
+                    DueDate = DateTime.UtcNow.AddDays(4),
+                    Status = DAL.Enums.Status.ToDo,
+                    CreatedAt = DateTime.UtcNow,
+                }
+            };
+
+            _mockRepositoryWrapper.Setup(r => r.ToDoTaskRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.ToDoTask, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.ToDoTask>, IIncludableQueryable<DAL.Entities.ToDoTask, object>>>()))
+                .ReturnsAsync(entities);
+            
+            _mockMapper.Setup(r => r.Map<IEnumerable<ToDoTaskDto>>(entities)).Returns(dtos);
+
+            var result = await _handler.Handle(new GetAllToDoTasksQuery(), CancellationToken.None);
+
+            result.IsSuccess.Should().BeTrue();
+                _mockRepositoryWrapper.Verify(r => r.ToDoTaskRepository.GetAllAsync(
+                    It.IsAny<Expression<Func<DAL.Entities.ToDoTask, bool>>>(),
+                    It.IsAny<Func<IQueryable<DAL.Entities.ToDoTask>, IIncludableQueryable<DAL.Entities.ToDoTask, object>>>()),
+                    Times.Once);
+
+            _mockMapper.Verify(m => m.Map<IEnumerable<ToDoTaskDto>>(entities), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnFailResult_WhenTasksNotFound()
+        {
+
+            _mockRepositoryWrapper.Setup(r => r.ToDoTaskRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.ToDoTask, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.ToDoTask>, IIncludableQueryable<DAL.Entities.ToDoTask, object>>>()))
+                .ReturnsAsync((IEnumerable<DAL.Entities.ToDoTask>)null);
+
+            var result = await _handler.Handle(new GetAllToDoTasksQuery(), CancellationToken.None);
+
+            result.IsSuccess.Should().BeFalse();
+
+            _mockRepositoryWrapper.Verify(r => r.ToDoTaskRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.ToDoTask, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.ToDoTask>, IIncludableQueryable<DAL.Entities.ToDoTask, object>>>()),
+                Times.Once);
+
+            _mockMapper.Verify(m => m.Map<IEnumerable<ToDoTaskDto>>(It.IsAny<IEnumerable<DAL.Entities.ToDoTask>>()), Times.Never);
+        }
+    }
+}
